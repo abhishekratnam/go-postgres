a07441b16061b4357710cd44d075aa8039979e05
root <anshu98@outlook.com>
2021-02-17 11:35:35 +0530

    Go-postgres

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"go-postgres/router"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := router.Router()
+	// fs := http.FileServer(http.Dir("build"))
+	// http.Handle("/", fs)
+	fmt.Println("Starting server on the port 8000...")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
